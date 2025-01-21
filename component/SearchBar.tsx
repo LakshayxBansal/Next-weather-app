13c8e14bd4147b85433e6ee3f3be5be59d8ecaad
Lakshay Bansal <lakshay012003@gmail.com>
2025-01-21 17:57:26 +0530

    Utilized formData

--- component/SearchBar.tsx
+++ component/SearchBar.tsx
@@ -6,24 +6,36 @@ import WeatherCard from "./WeatherCard";
 
 export default function SearchBar(){
 
-    const [city, setCity] = useState("");
+    // const cityInputRef  = useRef<HTMLInputElement>(null);
     const [weather, setWeather] = useState<any>({});
-    const [error, setError] = useState("");
 
 
-    const handleSearch = async() => {
-        if(city.length === 0){
+
+    const handleSearch = async(event :  React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget); // Create a FormData object from the form
+        const city = formData.get("city") as string;
+
+        // const city = cityInputRef.current?.value;
+
+        if(! city || city.length === 0){
             return alert("Please enter a city name");
         }
         if (!/^[a-zA-Z\s]+$/.test(city)) {
             return alert("Invalid city name. Please enter letters only.");
         }
 
+
         try{
+            setTimeout(async () => {
+                console.log("Timeout over");
+            }, 10000);
             const response = await axios.get(`/api/fetchWeather?city=${city}`);
+            
             setWeather(response.data);
-            setCity("");
+            // cityInputRef.current!.value = "";
         }catch (err: any) {
+            console.log(err);
             if (err.response && err.response.data.error) {
               alert(err.response.data.error);
             } else {
@@ -34,20 +46,20 @@ export default function SearchBar(){
 
     
     return (
-        <div>
+        <form onSubmit={handleSearch}>
             <input
             type = "text"
             placeholder="Enter city name"
             style={{ color: "black", backgroundColor: "white" }}
-            value = {city}
-            onChange = {(e) => setCity(e.target.value)}
+            name = "city"
+            // ref = {cityInputRef}
             />
 
-            <button onClick={handleSearch} style={{ padding: "10px 20px" }}>
+            <button type="submit" style={{ padding: "10px 20px" }}>
                 Search
             </button>
 
             <WeatherCard weather = {weather} />
-        </div>
+        </form>
     )
 }
